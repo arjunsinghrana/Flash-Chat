1c4529d37972e5f737acac32d756f3eea04fea98
Arjun <asingh105@jaguarlandrover.com>
2018-07-25 11:39:01 +0100

    Add SVProgressHUD to login and register views

--- Flash Chat/LogInViewController.swift
+++ Flash Chat/LogInViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import SVProgressHUD
 
 class LogInViewController: UIViewController {
 
@@ -25,7 +26,9 @@ class LogInViewController: UIViewController {
 
    
     @IBAction func logInPressed(_ sender: AnyObject) {
-
+        
+        SVProgressHUD.show()
+        
         //TODO: Log in the user
         Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (result, error) in
             
@@ -33,6 +36,9 @@ class LogInViewController: UIViewController {
                 print(error)
             } else {
                 print("Log In successful")
+                
+                SVProgressHUD.dismiss()
+                
                 self.performSegue(withIdentifier: "goToChat", sender: self)
             }
         }
